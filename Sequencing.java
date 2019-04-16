d7ed4f826fff4a544011c4f07dac2965c680e79a
ameliebaeck <49672738+ameliebaeck@users.noreply.github.com>
2019-04-16 11:31:47 +0200

    Add files via upload
    Added small case and updated big case

--- Sequencing.java
+++ Sequencing.java
@@ -9,28 +9,28 @@ import se.lth.control.realtime.DigitalOut;
 
 
 public class Sequencing extends Thread{
-	
-	
+
+
 	public static final int SMALL = 0;
 	public static final int MEDIUM = 1;
 	public static final int BIG = 2;
-	
-	
-	
+
+
+
 	private Regul regul;
 	private ReferenceGenerator refgen;
 	private ModeMonitor modeMon;
-	
+
 	private int priority;
 	private Semaphore mutex; // used for synchronization at shut-down
-	
+
 	private AnalogIn analogInAngle;
 	private AnalogIn analogInPosition;
 	private AnalogOut analogOut;
-	
+
 	private DigitalIn pitch;
 	private DigitalOut fire;
-	
+
 	//Inner monitor class
 	class ModeMonitor{
 		private int mode;
@@ -44,7 +44,7 @@ public class Sequencing extends Thread{
 			return mode;
 		}
 	}
-	
+
 	public Sequencing(int pri){
 		priority = pri;
 		mutex = new Semaphore(1);
@@ -60,36 +60,27 @@ public class Sequencing extends Thread{
 		}
 		modeMon = new ModeMonitor();
 	}
-	
+
 	public void setSMALLMode() {
 		modeMon.setMode(0);
 		System.out.println("SMALL ball detected");
 	}
-	
+
 	public void setMEDIUMMode() {
 		modeMon.setMode(1);
 		System.out.println("MEDIUM ball detected");
 	}
-	
+
 	public void setBIGMode() {
 		modeMon.setMode(2);
 		System.out.println("BIG ball detected");
 	}
-	
-	public void setRefGen(ReferenceGenerator referenceGenerator) {
-		// Written by you
-		this.referenceGenerator = referenceGenerator;
-	}
-	public void setRegul(Regul regul) {
-		// Written by you
-		this.Regul = regul;
-	}
-	
+
 	public void run() {
 		setPriority(priority);
 		mutex.take();
 		while(true) {
-		
+
 			//initial state
 			regul.setBEAMMode();
 			// set ref angle to 0
@@ -97,7 +88,7 @@ public class Sequencing extends Thread{
 			refgen.setRef(0.0);
 			try {
 				while(0.0 != analogInAngle.get()) {
-				}		
+				}
 			} catch (Exception e) {
 				System.out.println(e);
 			}
@@ -111,11 +102,11 @@ public class Sequencing extends Thread{
 				}
 			} catch (Exception e) {
 				System.out.println(e);
-		
+
 			}
-			
+
 			// push ball state
-			
+
 			try {
 				fire.set(true);
 				while(-10.0 != analogInPosition.get()) {
@@ -123,11 +114,11 @@ public class Sequencing extends Thread{
 			} catch (Exception e) {
 				System.out.println(e);
 			}
-			
+
 			// ball mode state
 			//set ballMode state true
 			regul.setBALLMode();
-			
+
 			//ball position state
 			//go to the measure position
 			refgen.setRef(5.0); // WE TRY WITH 5.0
@@ -138,7 +129,7 @@ public class Sequencing extends Thread{
 			} catch (Exception e) {
 				System.out.println(e);
 			}
-			
+
 			// measure size state
 			// measure the control signal to determine the size of the ball
 			double controlsignal = 0.0;
@@ -148,30 +139,45 @@ public class Sequencing extends Thread{
 			controlsignal = controlsignal/10.0;
 			if(controlsignal < 1) {
 				setSMALLMode();
-				
-				
+
+
 			} else if (1 < controlsignal && controlsignal < 2) {
 				setMEDIUMMode();
 			} else {
 				setBIGMode();
 			}
-			
+
 			switch (modeMon.getMode()) {
 			case SMALL: {
 				regul.setBEAMMode();
 				//small state
-				
-				
+				//increase angle
+				refgren.setRef(5);
+				try {
+					while(-7.0 < analogInPosition.get()) {
+					}
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				//decrease angle to throw in the small basket
+				refgren.setRef(-5);
+				try {
+					while(-9.5 < analogInPosition.get()) {
+					}
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				break;
 			}
 			case MEDIUM: {
-				
+
 			}
 			case BIG: {
 				//Drop ball on the floor
 				regul.setBEAMMode();
 				refgen.setRef(-5);
 				try {
-					while(3 > analogInPosition.get() || -4.5 > analogInAngle.get()) {
+					while(7 > analogInPosition.get() || -4.5 > analogInAngle.get()) {
 					}
 				} catch (Exception e) {
 					System.out.println(e);
@@ -181,5 +187,5 @@ public class Sequencing extends Thread{
 			}
 		}
 	}
-	
+
 }
