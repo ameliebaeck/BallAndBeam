0389ea15f1d11c826f5520ab6fb17fad2a8dbf67
42ea <erian@axis.com>
2019-04-16 10:29:22 +0200

    Added sequencing to main

--- Main.java
+++ Main.java
@@ -7,14 +7,20 @@ public class Main {
         final int regulPriority = 8; 
         final int refGenPriority = 6; 
         final int plotterPriority = 7; 
+        final int seqPriority = 5;
 
         ReferenceGenerator refgen = new ReferenceGenerator(refGenPriority); 
         Regul regul = new Regul(regulPriority); 
+        Sequencing seq = new Sequencing(seqPriority);
+
 	final  OpCom opcom = new OpCom(plotterPriority); // Must be declared final since it is used in an inner class
 
         regul.setOpCom(opcom); 
         regul.setRefGen(refgen);
- 
+
+        seq.setRefGen(refgen);
+        seq.setRegul(regul);
+
         opcom.setRegul(regul); 
 
 	Runnable initializeGUI = new Runnable(){
@@ -28,8 +34,9 @@ public class Main {
 	}catch(Exception e){
 	    return;
 	}
-
+        
         refgen.start(); 
         regul.start(); 
+        seq.start()
     } 
 } 
